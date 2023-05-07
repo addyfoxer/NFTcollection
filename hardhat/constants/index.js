28129fccbc746e692a5e8747f8ef45771dbcd29b
addyfoxer <adarshku12345@gmail.com>
2023-05-08 00:23:19 +0530

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x837accC38018634A08E0Bc776dbea5dbA561CA7e";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
